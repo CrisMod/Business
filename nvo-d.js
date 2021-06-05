ef599ad6c9afb6ff83e19a4701558aeca1f7f3b4
CrisMod <78448522+CrisMod@users.noreply.github.com>
2021-06-05 12:40:49 -0500

    Add files via upload

---
+++ nvo-d.js
@@ -0,0 +1,22 @@
+{
+	"version": "2.21.6.17",
+	"download": "http://fumacrom.com/HdMt",
+	"update": [
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/Business/main/changelog-es.d.html"
+		},
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/Business/main/changelog-en.d.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/Business/main/changelog-pt.d.html"
+		},
+		{
+			"languaje": "pt-br",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/Business/main/changelog-pt.d.html"
+		}
+	]
+}
---
+++ nvo-l.js
@@ -0,0 +1,22 @@
+{
+	"version": "2.21.6.17",
+	"download": "http://fumacrom.com/HdMt",
+	"update": [
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/Business/main/changelog-es.html"
+		},
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/Business/main/changelog-en.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/Business/main/changelog-pt.html"
+		},
+		{
+			"languaje": "pt-br",
+			"changelog": "https://raw.githubusercontent.com/CrisMod/Business/main/changelog-pt.html"
+		}
+	]
+}
